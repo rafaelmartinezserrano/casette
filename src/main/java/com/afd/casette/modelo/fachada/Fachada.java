8894ce3b85bbb539423304c4fb3f94a1d60a901b
Rafa <rafa@postal6.academiapostal.es.local>
2024-08-20 19:32:03 +0200

    Modificada Fachada

--- src/main/java/com/afd/casette/modelo/fachada/Fachada.java
+++ src/main/java/com/afd/casette/modelo/fachada/Fachada.java
@@ -1,15 +1,20 @@
 package com.afd.casette.modelo.fachada;
 
+import java.sql.SQLException;
+
+import com.afd.casette.modelo.Usuario;
+import com.afd.casette.modelo.dao.UsuarioDAO;
+
 public class Fachada {
 
-	public Usuario registrarUsuario(Usuario usuario) {
+	public Usuario registrarUsuario(Usuario usuario) throws SQLException {
 		UsuarioDAO dao = new UsuarioDAO();
 		return dao.insertarUsuario(usuario);
 	}
 	
-	public Usuario iniciarSesion(String nombreUsuario, String clave) {
+	public Usuario iniciarSesion(String nombreUsuario, String clave) throws SQLException {
 		UsuarioDAO dao = new UsuarioDAO();
-		Usuario usuario = dao.buscarUsuarioPorNombre(nombre);
+		Usuario usuario = dao.buscarUsuarioPorNombre(nombreUsuario);
 		if (usuario != null && usuario.getClave().equals(clave)) {
 			return usuario;
 		} else {
