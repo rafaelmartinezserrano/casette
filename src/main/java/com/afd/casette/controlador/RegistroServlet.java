7192588aa6e665ffa3520df3f2f6d55c5277e61c
AFD Tardes <afdt@postal6.academiapostal.es.local>
2024-08-20 20:14:13 +0200

    funcionalidad y registro usuario

---
+++ src/main/java/com/afd/casette/controlador/RegistroServlet.java
@@ -0,0 +1,52 @@
+package com.afd.casette.controlador;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import com.afd.casette.modelo.Usuario;
+import com.afd.casette.modelo.fachada.Fachada;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class RegistroServlet
+ */
+public class RegistroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nombreUsuario = request.getParameter("nombreUsuario");
+		String clave = request.getParameter("clave");
+		String email = request.getParameter("email");
+		String strfechaNacimiento = request.getParameter("fechaNacimiento");
+		LocalDate fechaNacimiento = LocalDate.parse(strfechaNacimiento);
+
+		Usuario usuario = new Usuario(0, nombreUsuario, clave, email, fechaNacimiento);
+		System.out.println(usuario);
+
+		Fachada fachada = new Fachada();
+		try {
+			usuario = fachada.registrarUsuario(usuario);
+			request.getSession().setAttribute(nombreUsuario, nombreUsuario);
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+			request.setAttribute("error", "error al registrar");
+			request.getRequestDispatcher("Registro.jsp");
+			
+		}
+	}
+
+}
---
+++ src/main/webapp/Registro.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Registro</title>
+</head>
+<body>
+
+<div>
+<form action="" method="post" autocomplete ="off">
+<ul>
+<li><input type= "email" name="email" required /></li>
+<li><input type = "password" name="clave" maxlength="25"required></li>
+<li><input type= "submit" name="submit" value="envía"></li>
+
+</ul>
+
+
+</form>
+</div>
+
+</body>
+</html>
