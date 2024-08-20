038366920e3e7cd17879e02d36db609672bc33a4
AFD Tardes <afdt@postal6.academiapostal.es.local>
2024-08-20 20:02:13 +0200

    Cambiados imports de iniciar sesion

--- src/main/java/com/afd/casette/controlador/IniciarSesionServlet.java
+++ src/main/java/com/afd/casette/controlador/IniciarSesionServlet.java
@@ -1,14 +1,16 @@
 package com.afd.casette.controlador;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
-import org.afd.catshop.modelo.fachada.Fachada;
+import com.afd.casette.modelo.Usuario;
+import com.afd.casette.modelo.fachada.Fachada;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 public class IniciarSesionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -27,12 +29,14 @@ public class IniciarSesionServlet extends HttpServlet {
 			
 			if (usuario != null) {
 				if (recordarUsuario != null) {
-					Cookie galleta = new Cookie("recordarUsuario", Integer.toString(usuario.getIdusuario()));
+					Cookie galleta = new Cookie("recordarUsuario", Integer.toString(usuario.getIdUsuario()));
 					galleta.setMaxAge(10*24*60*60);
 					response.addCookie(galleta);
 					
 				}
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
