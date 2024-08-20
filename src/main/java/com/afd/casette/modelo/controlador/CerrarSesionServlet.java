0b904ba2085cc2268079eca6f0199669b9b5c9f1
AFD Tardes <afdt@postal6.academiapostal.es.local>
2024-08-20 19:13:48 +0200

    Funcionalidad Cerrar Sesión

---
+++ src/main/java/com/afd/casette/modelo/controlador/CerrarSesionServlet.java
@@ -0,0 +1,26 @@
+package com.afd.casette.modelo.controlador;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CerrarSesionServlet
+ */
+public class CerrarSesionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Destruyo la sesión con el método invalidate 
+		request.getSession().invalidate();
+		//Voy a index.jsp
+		response.sendRedirect("principal.jsp");
+	}
+
+}
---
+++ src/main/webapp/principal.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+<meta charset="UTF-8">
+<title>Casette</title>
+
+</head>
+
+<body>
+
+<div>
+
+<a href="CerrarSesion"></a>
+
+</div>
+
+</body>
+
+</html>
